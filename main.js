eaf50e80990e6d0519282ffb9ad9fbb29642dcab
Daniel Mulvihill <dmulvihill3@gmail.com>
2018-08-13 15:41:16 -0400

    Added a fancy heading that moves to nav-bar when the user scrolls

--- main.js
+++ main.js
@@ -14,116 +14,72 @@ $.ajax({
 
       $(".row").append(
         "<div class='component'>"+
-        "<div class='item'>"+res.products[i].title+"</div>"+
-        "<div class='price'>$"+res.products[i].variants[0].price+"</div>"+
-        "<div class='img'><img src='"+ res.products[i].images[0].src +"'></div>"+
-        "<div class='description'>"+res.products[i].body_html+"</div>"+
-        "<button class='purchase'>Add to Cart</button>"+
+            "<div class='item'>"+res.products[i].title+"</div>"+
+            "<div class='price'>$"+res.products[i].variants[0].price+"</div>"+
+            "<div class='img'><img src='"+ res.products[i].images[0].src +"'></div>"+
+            "<div class='description'>"+res.products[i].body_html+"</div>"+
+            "<button class='purchase'>Add to Cart</button>"+
         "</div>"
       )
     }
   }
-})
+}) //end ajax
 
-
-// $('#container').html(
-// "<div class='component'>" + product.title + "</div>" +
-// "<span></span>"
-// )
 
   //mobile menu
 
-
-
     $(".mobile-menu_open").on("click", function(e){
-
       e.preventDefault();
-
       $(".top-nav ul").slideDown();
-
       $(".mobile-menu_open").hide();
-
       $(".mobile-menu_close").show();
 
     })
 
-
-
     $(".mobile-menu_close").on("click", function(e){
-
       e.preventDefault();
-
       $(".top-nav ul").slideUp();
-
       $(".mobile-menu_close").hide();
-
       $(".mobile-menu_open").show();
 
     })
 
-
-
-
-
-    //this code slides up the menu when user 
-
-    //scrolls or clicks on container, but only on mobile devices
+    //suppose to slide up the menu when user scrolls or clicks on container on mobile devices, but it's a little uncooperative
 
     if(innerWidth<=600){
-
       $(".container").on("click", function(){
-
-        
-
           $(".top-nav ul").slideUp();
-
           $(".mobile-menu_close").hide();
-
-          $(".mobile-menu_open").show();
-
-        
-
+          $(".mobile-menu_open").show();    
       })
-
-
 
       $(window).scroll(function(){
-
         $(".top-nav ul").slideUp();
-
         $(".mobile-menu_close").hide();
-
         $(".mobile-menu_open").show();
-
       })
-
     }
 
-  //scroll to top 
-
-
+  //scroll back to top 
 
     $(".js-top").on("click", function(e){
-
       e.preventDefault();
-
       scroll(0);
-
     });
 
- 
-
-    function scroll(top){
-
+    function scroll(y){
       $('html,body').animate({
-
-        scrollTop: top
-
-      },500)
-
+        scrollTop: y
+       },500)
     }
 
+//header appears on nav bar on scroll.
+
+    $(window).on("scroll", function(){
+      $(".top-nav h1 span").fadeIn();
+      $("header h1").hide();
+    })
 
 
-});
+}); //end document.ready
 
